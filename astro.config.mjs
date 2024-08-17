e923ce6c75aaa17476b4b8cf073748cb94fc2219
dracero <diego.racero@gmail.com>
2024-08-17 20:22:10 -0300

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
   experimental: {
-    serverIslands: true,
-  },
+    serverIslands: true
+  }
 });
